09642e01304681f7a4cb3d85963d2b6c38b58445
ziv <ziv@ziv.hypervisor>
2019-06-24 11:00:03 -0400

    init repo

---
+++ server.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <sys/epoll.h>
+
+
+using namespace std;
+
+class Server{
+public:
+    void init(int listenPort){
+        
+    }
+    
+    void start(){
+        // work loop
+        
+    }
+};
+
+int main(){
+    Server server;
+    server.init(7002);
+    server.start();
+    return 0;
+}
